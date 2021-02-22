94e955cc2341cb904cb6b545ca7401d42c998658
vparkash2014 <57243248+vparkash2014@users.noreply.github.com>
2021-02-22 22:42:26 +1100

    finished mvp

--- script/calculator.js
+++ script/calculator.js
@@ -22,14 +22,40 @@ const checkDecimal = (mathStr) => {
     let resultStr = '';
 
     if (!mathStr || mathStr.charAt(mathStr.length - 1).match(/[^0-9]/g)) {
-        resultStr += '0.';;
-    };
-
+        resultStr += '0.';
+    } else {
+        resultStr += '.';
+    }
+ 
     return resultStr;
 }
 
+// this function will check the history and clear it 
+const checkHistory = (historyStr) => {
+    console.log(historyStr, historyStr.match(/[-+*/]/g))
+
+    if (historyStr.charAt(historyStr.length - 1).match("=")) {
+        document.getElementById('output').innerHTML = "";
+        historyStr = "";
+
+    } else if (historyStr.match(/[-+*/]/g)) {
+        console.log('passed');
+        if (historyStr.match(/[-+*/]/g).length >= 2) {
+            console.log(`passed2`, historyStr.split(' '))
+            document.getElementById('output').innerHTML = `${historyStr.split(' ')[1]}${historyStr.split(' ')[2]}`;
+            historyStr = `${historyStr.split(' ')[1]}${historyStr.split(' ')[2]}`;
+        }
+    }
+
+    return historyStr;
+};
+
 const getInput = (event) => { 
     event.preventDefault();
+
+    if (historyStr) {
+        historyStr = checkHistory(historyStr);
+    }
 
     let inputVal = event.target.value;
     let str = document.getElementById('output').innerHTML;
@@ -37,20 +63,21 @@ const getInput = (event) => {
     if (str.match(/[-+*/=]/g) && inputVal.match(/[-+*/=]/g)) {
         const result = letDoMath(str);
         str += `<br /> ${result}`;
-        const nextOperator = inputVal;
+        historyStr += `= ${result} ${inputVal}`;
     } else {
-        if (inputVal === ".") {
-            inputVal = checkDecimal(str);
+        if (inputVal === '.') {
+            inputVal = checkDecimal(str) 
         };
 
         str += inputVal; 
+        historyStr += inputVal;
     }    
 
     document.getElementById('output').innerHTML = str;
 }
 
 const calculator = document.getElementById('calculator');
-
+let historyStr = "";
 
 calculator.addEventListener('click', getInput);
 
