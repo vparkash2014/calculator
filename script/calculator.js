597bad2d90965c7ff61eefc1c77da010196d2c9b
Vaishali Parkash <vaishaliparkash@MacBook.local>
2021-02-22 20:07:00 +1100

    building calculator

--- script/calculator.js
+++ script/calculator.js
@@ -1,7 +1,59 @@
-const showValue = () => {
-    const value = document.getElementById('btn').value;
-
-    document.getElementById('output').innerHTML = value;
-
-      
+const showValue = (event) => {
+    const val = event.target.value;
+    document.getElementById('output').innerHTML = val;
 }
+
+
+const letDoMath = (mathStr) => {
+    const values = mathStr.split(/[-+*/=]/g);
+    const operator = mathStr.match(/[-+*/=]/g).join('');
+    let result = 0;
+
+    console.log(values, parseFloat(values[0]), parseFloat(values[1]));
+    if (operator === '+') {
+        result = parseFloat(values[0]) + parseFloat(values[1]);
+    } else if (operator === '-') {
+        result = parseFloat(values[0]) - parseFloat(values[1]);
+    } else if (operator === '*') {
+        result = parseFloat(values[0]) * parseFloat(values[1]);
+    } else if (operator === '/') {
+        result = parseFloat(values[0]) / parseFloat(values[1]);
+    }
+
+    return result;
+}
+
+const checkDecimal = (mathStr) => {
+    let resultStr = '';
+
+    if (!mathStr || mathStr.charAt(mathStr.length - 1).match(/[^0-9]/g)) {
+        resultStr += '0.';
+        console.log(resultStr);
+    };
+
+    return resultStr;
+}
+
+const getInput = (event) => { 
+    event.preventDefault();
+    
+    let inputVal = event.target.value;
+    let str = document.getElementById('output2').innerHTML;
+
+    if (str.match(/[-+*/=]/g) && inputVal.match(/[-+*/=]/g)) {
+        const result = letDoMath(str);
+        document.getElementById('output2').innerHTML = `${result}`;
+    } else {
+        if (inputVal === ".") {
+            inputVal = checkDecimal(str);
+        };
+
+        document.getElementById('output2').innerHTML += inputVal; 
+    }    
+
+    console.log(document.getElementById('output2').innerHTML)
+}
+
+const calculator = document.getElementById('calculator');
+
+calculator.addEventListener('click', getInput);
